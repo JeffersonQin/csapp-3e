f986e1a3bd7277d22b0c709b28062a0103aa67e6
gyro永不抽风 <1247006353@qq.com>
2022-02-15 19:18:36 +0800

    feat: add 2.57

---
+++ homework/ch2/2.57.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++)
+	printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+void show_short(short x) {
+	show_bytes((byte_pointer) &x, sizeof(short));
+}
+
+void show_double(double x) {
+	show_bytes((byte_pointer) &x, sizeof(double));
+}
+
+int main() {
+	show_int(0x12345678);
+	show_float(0.123456789);
+	show_short(0x1234);
+	show_double(0.123456789012345);
+    return 0;
+}
